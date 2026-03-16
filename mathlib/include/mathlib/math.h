2e5d2c6c6a68ccd8eb63635a1a88c82396785fe4
Icodery-c <islamovilnur@icloud.com>
2026-03-16 16:43:20 +0300

    Fix integer overflow in math.h

--- mathlib/include/mathlib/math.h
+++ mathlib/include/mathlib/math.h
@@ -4,30 +4,45 @@
 
 namespace mathlib {
 
-  inline int AdditionOperation (int num1, int num2, int* error) {
+  inline int AdditionOperation(int num1, int num2, int* error) {
 
-    if ((num2 > 0 && num1 > INT_MAX - num2) || (num2 < 0 && num1 < INT_MIN - num2))
-    {  *error = 1; return 0; }
+    int result;
 
-    return num1 + num2;
+    if (__builtin_add_overflow(num1, num2, &result))
+    {
+        *error = 1;
+        return 0;
+    }
 
-  }
-
-  inline int SubtractionOperation (int num1, int num2, int* error) {
-
-    if ((num2 < 0 && num1 > INT_MAX + num2) || (num2 > 0 && num1 < INT_MIN + num2))
-    { *error = 1; return 0; }
-
-    return num1 - num2;
+    return result;
 
   }
 
-  inline int MultiplicationOperation (int num1, int num2, int* error) {
+  inline int SubtractionOperation(int num1, int num2, int* error) {
 
-    if (num1 != 0 && (num1 * num2) / num1 != num2)
-    { *error = 1; return 0; }
+    int result;
 
-    return num1 * num2;
+    if (__builtin_sub_overflow(num1, num2, &result))
+    {
+        *error = 1;
+        return 0;
+    }
+
+    return result;
+
+  }
+
+  inline int MultiplicationOperation(int num1, int num2, int* error) {
+
+    int result;
+
+    if (__builtin_mul_overflow(num1, num2, &result))
+    {
+        *error = 1;
+        return 0;
+    }
+
+    return result;
 
   }
 
@@ -41,19 +56,18 @@ namespace mathlib {
 
   inline int PowerOperation(int base, int exp, int* error) {
 
-    int result = 1;
+      int result = 1;
 
-    for (int i = 0; i < exp; i++) {
+      for (int i = 0; i < exp; i++)
+      {
+          if (__builtin_mul_overflow(result, base, &result))
+          {
+              *error = 1;
+              return 0;
+          }
+      }
 
-      if (base != 0 && (result * base) / base != result)
-      { *error = 1; return 0; }
-
-      result *= base;
-
-    }
-
-    return result;
-
+      return result;
   }
 
   inline int FactorialOperation (int num, int* error) {
@@ -66,7 +80,11 @@ namespace mathlib {
 
     if (*error) { return 0; }
 
-    if (result > INT_MAX / num) { *error = 1; return 0;}
+    if (__builtin_mul_overflow(result, n, &result))
+    {
+        *error = 1;
+        return 0;
+    }
 
     return result * num;
 
