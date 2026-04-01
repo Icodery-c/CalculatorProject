6aa549ff43ffa1226fb118d17bf9e108f010ce69
Icodery-c <islamovilnur@icloud.com>
2026-04-01 14:48:14 +0300

    feat(parser): add JSON parsing with overflow detection

--- calculator/src/parser.cpp
+++ calculator/src/parser.cpp
@@ -1,43 +1,49 @@
-#include <cstdlib>
-#include <unistd.h>
-#include <climits>
-#include <cerrno>
-#include <cstring>
 #include "parser.h"
 
-void parse(int argc, char **argv, Data *data) {
-  int opt;
-  long value;
-  char *endptr;
+#include <stdexcept>
+#include <string>
+#include <climits>
+#include <nlohmann/json.hpp>
 
-  while ((opt = getopt(argc, argv, "a:b:o:h")) != -1) {
-    switch (opt) {
-    case 'a':
-      errno = 0;
-      value = strtol(optarg, &endptr, 10);
-      if (errno == ERANGE || *endptr != '\0' || value > INT_MAX || value < INT_MIN) {
-        data->op = 'e';
-      } else {
-        data->num1 = (int)value;
-      }
-      break;
-    case 'b':
-      errno = 0;
-      value = strtol(optarg, &endptr, 10);
-      if (errno == ERANGE || *endptr != '\0' || value > INT_MAX || value < INT_MIN) {
-        data->op = 'e';
-      } else {
-        data->num2 = (int)value;
-      }
-      break;
-    case 'o':
-      if (data->op == 'e') break; 
-      if (optarg[0] == '*' || strlen(optarg) > 1) { data->op = '*'; }
-      else { data->op = optarg[0]; }
-      break;
-    case 'h':
-      data->op = 'h';
-      break;
+using json = nlohmann::json;
+
+static inline int parseJsonInt (const json& j, const std::string& field) {
+
+    if (!j[field].is_number_integer()) { throw std::runtime_error("Field '" + field + "' is not an integer"); }
+
+    auto value = j[field].get<long long>();
+    
+    if (value > INT_MAX || value < INT_MIN) { throw std::runtime_error("Field '" + field + "' overflows int"); }
+
+    return static_cast<int>(value);
+
+}
+
+Data Parser::parse(int argc, char **argv) {
+
+    if (argc < 2) { throw std::runtime_error("There is no input data in JSON format."); }
+
+    json jobj = json::parse(argv[1]);
+
+    if (!jobj.contains("op")) { throw std::runtime_error("Missing required field: op"); }
+
+    Data data;
+
+    data.op = jobj["op"].get<std::string>()[0];
+
+    if (data.op == 'h') { return data; }
+
+    if (!jobj.contains("num1")) { throw std::runtime_error("Missing required field: num1"); }
+
+    data.num1 = parseJsonInt(jobj, "num1");
+
+    if (data.op != '!') {
+
+        if (!jobj.contains("num2")) { throw std::runtime_error("num2 is required for arithmetic operations."); }
+
+        data.num2 = parseJsonInt(jobj, "num2");
+
     }
-  }
+
+    return data;
 }
