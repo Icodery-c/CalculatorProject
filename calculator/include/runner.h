06427df611f46d762a86cfa3e0b583a24e80fa82
Icodery-c <islamovilnur@icloud.com>
2026-04-02 16:11:08 +0300

    feat(calculator) listed rule 5 and declared Parser, Checker, Calculator, Printer as class fields

--- calculator/include/runner.h
+++ calculator/include/runner.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include "parser.h"
+#include "checker.h"
+#include "calculator.h"
+#include "printer.h"
+
 class Runner {
 
-    public:
-    
-        void run (int argc, char **argv);
+    Parser parser;
+    Checker checker;
+    Calculator calculator;
+    Printer printer;
+
+public:
+    Runner() = default;
+    ~Runner() = default;
+
+    Runner(const Runner&) = default;
+    Runner& operator=(const Runner&) = default;
+
+    Runner(Runner&&) = default;
+    Runner& operator=(Runner&&) = default;
+
+    void run(int argc, char** argv);
 };
