705795c60fc318e4605874395ef40ee8f68f9bdd
Icodery-c <islamovilnur@icloud.com>
2026-04-02 16:18:11 +0300

    feat(calculator) declared Parser, Checker, Calculator, Printer as class fields

--- calculator/src/runner.cpp
+++ calculator/src/runner.cpp
@@ -1,30 +1,20 @@
 #include "runner.h"
-#include "parser.h"
-#include "checker.h"
-#include "calculator.h"
-#include "printer.h"
 #include "logger.h"
 
 #include <exception>
 
 void Runner::run(int argc, char** argv)
 {
-    Parser parser;
-    Checker checker;
-    Calculator calculator;
-    Printer printer;
-
     auto& log = logger::Logger::Initialize();
 
     try
     {
-
         log.info("Starting calculator application");
 
         log.debug("Parsing input...");
         Data data = parser.parse(argc, argv);
 
-        if (data.op == 'h') { printer.printHelp(); return; }
+        if (data.op == Operation::Help) { printer.printHelp(); return; }
 
         log.debug("Validating data...");
         checker.check(data);
@@ -34,7 +24,7 @@ void Runner::run(int argc, char** argv)
 
         log.info("Result: " + std::to_string(result));
         printer.printResult(result);
-    
+
     } catch (const std::exception& e) {
 
         log.error(std::string("Unexpected error: ") + e.what());
